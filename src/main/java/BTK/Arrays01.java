1c01ca882ece478f1eadbe8565694ed9ffbc0bde
Merve SEÇİNTİ ŞAHAN <secintimerve27@gmail.com>
2023-05-24 12:34:34 +0300

    24.05.2023 my studies2

---
+++ src/main/java/BTK/Arrays01.java
@@ -0,0 +1,27 @@
+package BTK;
+import java.util.Arrays;
+
+public class Arrays01 {
+    public static void main(String[] args) {
+
+        String[] ogrenciler= new String[4];
+    ogrenciler[0] ="Engin";
+    ogrenciler[1] ="Derin";
+    ogrenciler[2] ="Salih";
+    ogrenciler[3] ="Salih";
+
+        System.out.println(Arrays.toString(ogrenciler));
+
+        for (int i=0; i<ogrenciler.length; i++){
+            System.out.println(ogrenciler[i]);
+        }
+
+
+
+
+
+
+    }
+
+
+}
